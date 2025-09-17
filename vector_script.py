8c3ab94bb7bf7ffd9a62a0abcdc9ad78ff2c2406
H <harrisonsheldon@outlook.com>
2025-09-17 15:28:12 -0700

    working rag script

--- vector_script.py
+++ vector_script.py
@@ -7,10 +7,13 @@ import ollama
 # ollama models 
 EMBEDDING_MODEL = 'hf.co/CompendiumLabs/bge-base-en-v1.5-gguf'
 LANGUAGE_MODEL = 'hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF'
+
+# Load the dataset
+
 dataset = []
-
-
-
+with open('cat.txt', 'r') as file:
+  dataset = file.readlines()
+  print(f'Loaded {len(dataset)} entries')
 
 # Implement Vector Database  ***********
 
@@ -25,6 +28,10 @@ def add_chunk_to_database(chunk):
   embedding = ollama.embed(model=EMBEDDING_MODEL, input=chunk)['embeddings'][0]
   VECTOR_DB.append((chunk, embedding))
 
+for i, chunk in enumerate(dataset):
+    add_chunk_to_database(chunk)
+print(f'Added chunk {i+1}/{len(dataset)} to the database')
+
 
 # Retrieval functions ***************
 
@@ -35,6 +42,7 @@ def cosine_similarity(a, b):
   norm_a = sum([x ** 2 for x in a]) ** 0.5
   norm_b = sum([x ** 2 for x in b]) ** 0.5
   return dot_product / (norm_a * norm_b)
+
 
 def retrieve(query, top_n=3):
   query_embedding = ollama.embed(model=EMBEDDING_MODEL, input=query)['embeddings'][0]
@@ -49,19 +57,33 @@ def retrieve(query, top_n=3):
   return similarities[:top_n]
 
 
+# Generation phase ************
 
-def main():
-    # load cat dataset into a list of strings 
-    with open('cat.txt', 'r') as file:
-        dataset = file.readlines()
-    # successfull loading of dataset
-    print(f'Loaded {len(dataset)} entries')
+# Chatbot 
 
-    # add each chunk to the database from the cat dataset
-    for i, chunk in enumerate(dataset):
-        add_chunk_to_database(chunk)
-    print(f'Added chunk {i+1}/{len(dataset)} to the database')
+input_query = input('Ask me a question: ')
+retrieved_knowledge = retrieve(input_query)
 
+print('Retrieved knowledge:')
+for chunk, similarity in retrieved_knowledge:
+  print(f' - (similarity: {similarity:.2f}) {chunk}')
 
-if __name__ == "__main__":
-    main()
+instruction_prompt = f'''You are a helpful chatbot.
+Use only the following pieces of context to answer the question. Don't make up any new information:
+{'\n'.join([f' - {chunk}' for chunk, similarity in retrieved_knowledge])}
+'''
+#print(instruction_prompt)
+
+stream = ollama.chat(
+  model=LANGUAGE_MODEL,
+  messages=[
+    {'role': 'system', 'content': instruction_prompt},
+    {'role': 'user', 'content': input_query},
+  ],
+  stream=True,
+)
+
+# print the response from the chatbot in real-time
+print('Chatbot response:')
+for chunk in stream:
+  print(chunk['message']['content'], end='', flush=True)
